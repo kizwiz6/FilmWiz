d5e14aef1792946e07116c402e1665904c6e5148
Kieran Emery <kizwiz@hotmail.co.uk>
2025-01-28 13:29:20 +0000

    - Add custom exception handling with the FilmSearchException class. This improves error message clarity.

---
+++ FilmWiz.Infrastructure/FilmSearchException.cs
@@ -0,0 +1,13 @@
+﻿namespace FilmWiz.Infrastructure
+{
+    /// <summary>
+    /// Exception thrown when film search operations fail
+    /// </summary>
+    public class FilmSearchException : Exception
+    {
+        public FilmSearchException(string message, Exception? innerException = null)
+            : base(message, innerException)
+        {
+        }
+    }
+}
